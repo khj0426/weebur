3548de4226133ae8029707d707c670626d22e343
kimhyojung <706shin1728@naver.com>
2025-05-31 14:59:51 +0900

    chore : ky라이브러리 설치

---
+++ src/app/services/api/index.ts
@@ -0,0 +1,13 @@
+import ky from "ky";
+export const api = ky.create({
+  prefixUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  timeout: 10000,
+  hooks: {
+    //요청 전 실행할 항목들 명시.
+    beforeRequest: [
+      (request) => {
+        request.headers.set("Content-Type", "application/json");
+      },
+    ],
+  },
+});
